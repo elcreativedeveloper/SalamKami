49adb790186dc317fc2a2fd4518850f67ca244ad
Yasya El Hakim <yasyaelhakim@gmail.com>
2022-09-28 18:58:05 +0700

    create 4 files and update 1 file

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: [
+        './dev/partials/layout/**/*.{xml,html,js}',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                fontPrimary: 'var(--fontPrimary)',
+                fontSecondary: 'var(--fontSecondary)',
+            },
+            backgroundImage: {
+                imageBackground: 'var(--imageBackground)',
+                imageOrnamentTop: 'var(--imageOrnamentTop)',
+                imageOrnamentBottom: 'var(--imageOrnamentBottom)',
+                imageOrnamentAltTop: 'var(--imageOrnamentAltTop)',
+                imageOrnamentAltBottom: 'var(--imageOrnamentAltBottom)',
+            },
+            colors: {
+                colorMain: 'var(--colorMain)',
+                colorText: 'var(--colorText)',
+                colorBackground: 'var(--colorBackground)',
+            },
+        },
+    },
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+        require('@tailwindcss/line-clamp')
+    ],
+};
