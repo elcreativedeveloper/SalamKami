43eaae1c9ef62043caff6a769cb3128d283f4223
Yasya El Hakim <yasyaelhakim@gmail.com>
2022-12-04 04:59:14 +0700

    update 16 files and create 2 files

--- dev/scripts/firebase.js
+++ dev/scripts/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { addDoc, doc, getFirestore, increment, onSnapshot, setDoc, updateDoc } from "firebase/firestore";
+import { addDoc, collection, doc, getFirestore, increment, onSnapshot, orderBy, query, serverTimestamp, setDoc, updateDoc } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAHOv64OFwobOVReOdO7I0FDn14ALio4Sk",
@@ -10,18 +10,28 @@ const firebaseConfig = {
     appId: "1:710379476608:web:9463f2d56ca7690f0eb865"
 };
 
+const formatDate = (dateData) => {
+    const day = dateData.toDate().getDate();
+    const month = dateData.toDate().toLocaleString('default', { month: 'long' });
+    const year = dateData.toDate().getFullYear();
+
+    return `${month} ${day}, ${year}`;
+};
+
 Defer(() => {
     initializeApp(firebaseConfig);
 
     const firestoreDatabase = getFirestore();
     const urls = window.location.href.split('?')[0];
 
-    onSnapshot(doc(firestoreDatabase, `web/${urls.split('/').pop()}`), (doc) => {
+    const subsViews = onSnapshot(doc(firestoreDatabase, `web/${urls.split('/').pop()}`), (doc) => {
         const docData = doc.data();
 
         const elementViews = document.getElementById('views');
         elementViews.querySelector('.view_eye').innerHTML = docData.views
-        elementViews.classList.remove('invisible')
+        elementViews.classList.remove('invisible');
+
+        !doc.metadata.hasPendingWrites && subsViews();
     })
 
     const buttonSwipe = document.getElementById('button_swipe');
@@ -31,5 +41,48 @@ Defer(() => {
         }, {
             merge: true
         });
+
+        
+        const subsMessages = onSnapshot(query(collection(firestoreDatabase, `web/${urls.split('/').pop()}/messages`), orderBy('message_date', 'desc')), (coll) => {
+            let templates = '';
+            coll.docs.forEach((colls) => {
+                const collsData = colls.data();
+                const msgName = collsData.message_name;
+                const msgContent = collsData.message_contant;
+                const msgDate = formatDate(collsData.message_date);
+
+                templates += `<div class='animate_animated animate_fadeInUp animate_slower bg-colorBackground relative mb-3 flex w-full flex-col items-start justify-center border border-colorMain p-3 rounded-md last:mb-0'><div class='mb-1'><strong>${msgName}</strong> - <em class='text-sm'>${msgDate}</em> </div>
+                <div>${msgContent}</div></div>`
+            });
+
+            document.getElementById('all_message').innerHTML += templates;
+
+            !coll.metadata.hasPendingWrites && subsMessages();
+        })
+    });
+
+    const buttonAddMessage = document.getElementById('button_add_message');
+    const forms = document.getElementById('form_rsvp');
+    buttonAddMessage.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        forms.classList.remove('hidden');
+        buttonAddMessage.classList.add('hidden');
+    })
+
+    let formClicker = 0;
+    forms && forms.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        ++formClicker;
+        if (formClicker == 1) {
+            addDoc(collection(firestoreDatabase, `web/${urls.split('/').pop()}/messages`), {
+                message_name: this.input_rsvp_name.value,
+                message_contant: decodeURI(this.input_rsvp_message.value),
+                message_date: serverTimestamp(),
+            }).then(() => {
+                forms.remove()
+            });
+        }
     })
 }, 3000)
