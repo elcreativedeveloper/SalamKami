e7daea2204941a441ddf9b1d116f8194bf6d8ff1
Yasya El Hakim <yasyaelhakim@gmail.com>
2022-12-04 00:54:21 +0700

    create 2 files and update 4 files

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,89 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
+const terserWebpackPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'build/scripts/script-head.js'),
-    output: {
-        path: path.resolve(__dirname, 'build/scripts'),
-        filename: 'bundle.js',
+    mode: 'production',
+    entry: {
+        // auth: './plugins/auth/scripts/script.js',
+        "firebase": './dev/scripts/firebase.js',
+        // midtrans: './plugins/midtrans/scripts/script.js',
+        // drive: './plugins/drive/scripts/script.js',
+        // main: './plugins/scripts.js',
+        // scripts: './src/assets/scripts/module.js',
     },
-};
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].css',
+                        },
+                    },
+                    {
+                        loader: 'extract-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    autoprefixer()
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            // Prefer Dart Sass
+                            implementation: require('sass'),
+
+                            // See https://github.com/webpack-contrib/sass-loader/issues/804
+                            webpackImporter: false,
+                            sassOptions: {
+                                includePaths: ['./node_modules']
+                            },
+                        },
+                    },
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new terserWebpackPlugin({
+                extractComments: false,
+                terserOptions: {
+                    mangle: true,
+                    compress: true,
+                    format: {
+                        comments: false,
+                    },
+                },
+            })
+        ],
+        // splitChunks: {
+        //     chunks: 'all',
+
+        // },
+    },
+    output: {
+        clean: true,
+        path: path.resolve(__dirname, './docs/assets/scripts'),
+        filename: '[name].js'
+    },
+    watch: true
+}
